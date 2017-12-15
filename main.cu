4dedbfaf324918a3fe54aa0401f06268720c6eb8
blakezuo <2015201974@ruc.edu.cn>
2017-12-15 22:02:57 +0800

    ??

--- main.cu
+++ main.cu
@@ -97,12 +97,12 @@ __global__ void spmv(int* row, int* col, float* data, float* vec, float* res, in
     }
     sum[0][warp] = tmp;
     __syncthreads();
-    // int times = 1,l = WARP_SIZE / 2;
-    // while(warp / l == 0)
-    // {
-    //     times ++;
-    //     l /= 2;
-    // }
+    int times = 1,l = WARP_SIZE / 2;
+    while(warp / l == 0)
+    {
+        times ++;
+        l /= 2;
+    }
     // int scale = WARP_SIZE / 2;
     // for(int j=1;j<=times;j ++)
     // {
